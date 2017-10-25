b06d389a870f1f08e6f0edee01c2b6bfce099994
Matthew Thor <mjthor@gmail.com>
2017-10-25 13:54:22 -0500

    updated sequelize connection string

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const pkg = require('../package.json');
 
 const name = process.env.DATABASE_NAME || pkg.name;
-const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${name}`;
+const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`;
 
 console.log(chalk.yellow(`Opening database connection to ${connectionString}`));
 
